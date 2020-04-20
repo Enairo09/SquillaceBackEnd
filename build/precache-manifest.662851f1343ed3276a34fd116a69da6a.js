38c63eedc56632412402335e9e5e24e0888ec98c
Oriane Mathet <oriane.mathet@gmail.com>
2020-04-20 16:17:47 +0200

    static files

---
+++ build/precache-manifest.662851f1343ed3276a34fd116a69da6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffef0b974e5d9b17851ffbe3381c32ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "373c14b7a91bb8e109ad",
+    "url": "/static/css/main.570834fb.chunk.css"
+  },
+  {
+    "revision": "c38f11d423a2b8ce0986",
+    "url": "/static/js/2.7ca6da39.chunk.js"
+  },
+  {
+    "revision": "89853604ac208a535d541c08c7c71648",
+    "url": "/static/js/2.7ca6da39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "373c14b7a91bb8e109ad",
+    "url": "/static/js/main.7035e759.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
